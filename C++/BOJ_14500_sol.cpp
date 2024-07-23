1fdf70b79b42665cdbbd929436eabe1b76e639fa
이익준 <76618299+IJLee0812@users.noreply.github.com>
2024-07-23 14:08:28 +0900

    Update BOJ_14500_sol.cpp
    sourcecode modification

--- C++/BOJ_14500_sol.cpp
+++ C++/BOJ_14500_sol.cpp
@@ -1,58 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dx[] = {-1, 0, 0, 1}, dy[] = {0, -1, 1, 0}, Map[501][501], visited[501][501], N, M, res = 0;
+int Map[501][501], visited[501][501], res = 0, dx[4] = {-1, 1, 0, 0}, dy[4] = {0, 0, -1, 1}, N, M;
 
-void solve(int x, int y, int cnt, int curScore){
+void DFS(int x, int y, int cnt, int sum){
     if (cnt == 4){ // 재귀 종료조건
-        if (res < curScore) res = curScore;
-        return;
+        if (res < sum) res = sum;
+        return; // 재귀 종료
     }
 
     for (int i = 0 ; i < 4 ; ++i){
         int nx = x + dx[i], ny = y + dy[i];
-
-        if (nx < 1 || ny < 1 || nx > N || ny > M || visited[nx][ny]) continue;
-
-        visited[nx][ny] = 1;
-
-        solve(nx, ny, cnt + 1, curScore + Map[nx][ny]);
-
-        visited[nx][ny] = 0; // 백트래킹
-    }
-
-    if (x - 1 >= 1 && x + 1 <= N && y - 1 >= 1) // ㅓ
-        res = max(res, Map[x - 1][y] + Map[x][y - 1] + Map[x][y] + Map[x + 1][y]);
-    
-    if (x - 1 >= 1 && x + 1 <= N && y + 1 <= M) // ㅏ
-        res = max(res, Map[x - 1][y] + Map[x][y + 1] + Map[x][y] + Map[x + 1][y]);
-    
-    if (x + 1 <= N && y - 1 >= 1 && y + 1 <= M) // ㅗ
-        res = max(res, Map[x][y] + Map[x + 1][y] + Map[x + 1][y - 1] + Map[x + 1][y + 1]);
-
-    if (x + 1 <= N && y - 1 >= 1 && y + 1 <= M)
-        res = max(res, Map[x][y - 1] + Map[x][y] + Map[x][y + 1] + Map[x + 1][y]);
-}
-
-int main(){
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-    cin >> N >> M;
-    
-    for (int i = 1 ; i <= N ; ++i)
-        for (int j = 1 ; j <= M ; ++j)
-            cin >> Map[i][j];
-        
-    for (int i = 1 ; i <= N ; ++i){
-        for (int j = 1 ; j <= M ; ++j){
-            visited[i][j] = 1;
-            
-            solve(i, j, 1, Map[i][j]);
-
-            visited[i][j] = 0; // 백트래킹
+        if (nx >= 1 && nx <= N && ny >= 1 && ny <= M && !visited[nx][ny]){
+            visited[nx][ny] = 1;
+            DFS(nx, ny, cnt + 1, sum + Map[nx][ny]);
+            visited[nx][ny] = 0; // Backtracking
         }
     }
 
-    cout << res;
+    // ㅓ
+    if (x - 1 >= 1 && x + 1 <= N && y - 1 >= 1)
+        res = max(res, Map[x][y] + Map[x - 1][y] + Map[x + 1][y] + Map[x][y - 1]);
+
+    // ㅏ
+    if (x - 1 >= 1 && x + 1 <= N && y + 1 <= M)
+        res = max(res, Map[x][y] + Map[x][y + 1] + Map[x - 1][y] + Map[x + 1][y]);
+
+    // ㅜ
+    if (x + 1 <= N && y - 1 >= 1 && y + 1 <= M)
+        res = max(res, Map[x][y] + Map[x + 1][y] + Map[x][y - 1] + Map[x][y + 1]);
+
+    // ㅗ
+    if (x - 1 >= 1 && y - 1 >= 1 && y + 1 <= M)
+        res = max(res, Map[x][y] + Map[x][y - 1] + Map[x][y + 1] + Map[x - 1][y]);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    cin >> N >> M;
+
+    for (int i = 1 ; i <= N ; ++i)
+        for (int j = 1 ; j <= M ; ++j)
+            cin >> Map[i][j];
+
+    for (int i = 1 ; i <= N ; ++i){
+        for (int j = 1 ; j <= M ; ++j){
+            visited[i][j] = 1;
+            DFS(i, j, 1, Map[i][j]);
+            visited[i][j] = 0; // Backtracking
+        }
+    }
+
+    cout << res << '\n';
+
     return 0;
 }
