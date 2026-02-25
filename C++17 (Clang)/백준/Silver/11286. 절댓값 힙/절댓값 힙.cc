a9739437ba2bb8ac7caafe85bc373d27f7a4e6c5
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-02-25 20:23:48 +0900

    [Silver I] Title: 절댓값 힙, Time: 16 ms, Memory: 2632 KB -BaekjoonHub

---
+++ C++17 (Clang)/백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct cmp{
+    bool operator()(int a, int b){
+        if (abs(a) == abs(b)) return a > b;
+        return abs(a) > abs(b);
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    priority_queue<int, vector<int>, cmp> pq;
+    vector<int> v;
+
+    int N, x; cin >> N;
+
+    for (int i = 0 ; i < N ; ++i){
+        cin >> x;
+        if (x == 0){
+            if (pq.empty()) v.push_back(0);
+            else{
+                v.push_back(pq.top());
+                pq.pop();
+            }
+        }
+        else pq.push(x);
+    }
+
+    for (int i = 0 ; i < v.size() ; ++i) cout << v[i] << '\n';
+
+    return 0;
+}
