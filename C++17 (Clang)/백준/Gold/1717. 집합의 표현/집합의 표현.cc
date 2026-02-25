8ab2d6b0474af7bd33ff493a7ed1d200f7a304b2
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-02-25 20:10:22 +0900

    [Gold V] Title: 집합의 표현, Time: 40 ms, Memory: 6772 KB -BaekjoonHub

---
+++ C++17 (Clang)/백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 1000001
+
+int parent[MAX], N, M, op, a, b;
+
+int find(int x){
+    if (x == parent[x]) return x;
+    return parent[x] = find(parent[x]);
+}
+
+void merge(int x, int y){
+    x = find(x);
+    y = find(y);
+    if (x > y) parent[x] = y;
+    else parent[y] = x;
+}
+
+void isUnion(int x, int y){
+    x = find(x);
+    y = find(y);
+    if (x == y) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+    cin >> N >> M;
+
+    for (int i = 0 ; i <= N ; ++i) parent[i] = i;
+
+    while (M--){
+        cin >> op >> a >> b;
+        if (op == 0) merge(a, b);
+        else isUnion(a, b);
+    }
+
+    return 0;
+}
