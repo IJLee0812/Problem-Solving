7aa6944f1445cecfaf3b32f131b087a83f81c027
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-02-25 20:29:05 +0900

    [Silver III] Title: Four Squares, Time: 12 ms, Memory: 2248 KB -BaekjoonHub

---
+++ C++17 (Clang)/백준/Silver/17626. Four Squares/Four Squares.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int D[50001];
+int main(){
+    int n; cin >> n;
+    for (int i = 1 ; i <= n ; ++i){
+        D[i] = D[i-1] + 1;
+        for (int j = 1 ; j * j <= i ; ++j)
+            D[i] = min(D[i], D[i-j*j] + 1);
+    }
+    cout << D[n]; 
+    return 0;
+}
