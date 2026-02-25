efe1b573907cd54fa1c8522acbb6bad039bb668d
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-02-25 20:21:03 +0900

    [Silver IV] Title: 제로, Time: 12 ms, Memory: 1504 KB -BaekjoonHub

---
+++ C99/백준/Silver/10773. 제로/제로.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define MAX 100000
+int stack[MAX];
+int top = -1;
+void push(int data) {
+    if (top >= MAX - 1) return;
+    stack[++top] = data;
+}
+void pop() {
+    if (top < 0) return;
+    stack[top--] = 0;
+}
+int main() {
+    int k, data, sum = 0;
+    scanf("%d", &k);
+    while (k--) {
+        scanf("%d", &data);
+        if (data == 0) pop();
+        else push(data);
+    }
+    for (int i = 0; i <= top; i++)
+        sum += stack[i];
+    printf("%d\n", sum);
+    return 0;
+}
