3a2513aa2abbd5a540141a229b4f95b6b0eaea9c
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-02-25 20:27:08 +0900

    [Silver III] Title: N과 M (3), Time: 488 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Silver/15651. N과 M （3）/N과 M （3）.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+int n, m;
+int arr[10];
+bool isused[10];
+void f(int k){
+	if (k==m){
+		for (int i = 0 ; i < m ; ++i)
+			printf("%d ", arr[i]);
+		printf("\n");
+		return;
+	}
+	for (int i = 1 ; i <= n ; ++i){
+		//if (!isused[i]){
+			arr[k] = i;
+			isused[i] = 1;
+			f(k+1);
+			isused[i] = 0;
+		//}
+	}
+}
+int main(){
+
+	scanf("%d %d", &n, &m);	
+
+	f(0);
+
+	return 0;
+}
