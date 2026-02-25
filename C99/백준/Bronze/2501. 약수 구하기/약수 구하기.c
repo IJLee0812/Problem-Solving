66b375fa012cc752bd3eefc180c85a7229087cac
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-02-25 20:13:38 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ C99/백준/Bronze/2501. 약수 구하기/약수 구하기.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+	int n,k; scanf("%d %d",&n,&k);
+	int cnt = 0;
+	for (int i = 1 ; i <= n ; ++i){
+		if (n%i == 0){
+			cnt++;
+			if(cnt==k){ 
+				printf("%d", i); 
+				break;
+			}
+		}
+        if (i == n && cnt < k) printf("0\n");
+	}
+	return 0;
+}
