b97086a70f989c781ed8483f056efed13361c396
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-02-25 20:24:34 +0900

    [Bronze I] Title: ROT13, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/11655. ROT13/ROT13.py
@@ -0,0 +1,19 @@
+answer = ''
+
+for i in input():
+    if i.isupper():
+        if (65 <= ord(i) <= 77):
+            answer += chr(ord(i) + 13) 
+        else:
+            answer += chr(ord(i) - 13) 
+    
+    elif i.islower():
+        if (97 <= ord(i) <= 109):
+            answer += chr(ord(i) + 13) 
+        else:
+            answer += chr(ord(i) - 13) 
+    
+    else:
+        answer += i
+        
+print(answer)
