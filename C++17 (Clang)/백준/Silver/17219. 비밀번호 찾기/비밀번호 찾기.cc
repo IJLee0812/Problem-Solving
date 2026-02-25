fa99c00a4a070ec336d396a5206da5d9f5c4adf0
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-02-25 20:28:42 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 240 ms, Memory: 16740 KB -BaekjoonHub

---
+++ C++17 (Clang)/백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+using namespace std;
+int main(){
+    cin.tie(NULL);
+    ios::sync_with_stdio(0);
+    int N, M; cin >> N >> M;
+    map<string, string> Map;
+    string x, y;
+    for (int i = 0 ; i < N ; ++i){
+        cin >> x >> y;
+        Map[x] = y;
+    }
+    for (int i = 0 ; i < M ; ++i){
+        cin >> x;
+        cout << Map[x] << '\n';
+    }
+    return 0;
+}
