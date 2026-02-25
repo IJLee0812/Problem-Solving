9bfff8115a7bbcffc25286b25ab28d398cc4fb73
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-02-25 20:27:48 +0900

    [Silver II] Title: N과 M (10), Time: 0 ms, Memory: 1156 KB -BaekjoonHub

---
+++ C99/백준/Silver/15664. N과 M （10）/N과 M （10）.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+int n, N = 0, m;
+int arr[10], num[10];
+bool isused[10];
+int nused[10000];
+void f(int k){
+	int tmp = 0;
+	if (k == m){
+		for (int i = 0 ; i < m ; ++i){
+			if (tmp > arr[i])
+				return;
+			tmp = arr[i];
+		}
+		for (int i = 0 ; i < m ; ++i)
+			printf("%d ", arr[i]);
+		printf("\n");
+		return;	
+	}
+	int prev = -1; 
+	for (int i = 1 ; i <= N ; ++i){
+		if (!isused[i] && prev != num[i-1]){
+			arr[k] = num[i-1];
+			isused[i] = 1;
+			prev = num[i-1];
+			f(k+1);
+			isused[i] = 0;
+		}
+	}	
+}
+int compare(const void *a, const void *b){
+	int A = *(int *)a;
+	int B = *(int *)b;
+	if (A < B) return -1;
+	else if (A > B) return 1;
+	return 0;	
+}
+int main(){
+	scanf("%d %d", &n, &m);
+	for (int i = 0 ; i < 10 ; ++i)
+		num[i] = 10001;
+	for (int i = 0 ; i < n ; ++i){
+		scanf("%d", &num[i]);
+		nused[num[i]-1]++;
+	}	
+	for (int i = 0 ; i < 10 ; ++i)
+		if (num[i] != 10001)
+			N++;
+	qsort(num, n, sizeof(int), compare);
+	f(0);
+	return 0;
+}
