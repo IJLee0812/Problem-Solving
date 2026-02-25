7642573c040a3b0b4b7d7db37cf898b5e764b927
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-02-25 20:22:01 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 1156 KB -BaekjoonHub

---
+++ C99/백준/Silver/10845. 큐/큐.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <string.h> 
+#define MAX 10001
+int queue[MAX];
+int head = 0, tail = 0;
+void push(int x){
+	queue[tail++] = x;
+}
+void pop(){
+	if (head == tail){
+		printf("-1\n");	
+	}
+	else{
+		printf("%d\n", queue[head++]);
+	}
+}
+void size(){
+	printf("%d\n", tail-head);
+}
+void empty(){
+	if (head == tail){
+		printf("1\n");
+	}
+	else printf("0\n");
+}
+void front(){
+	if (head == tail){
+		printf("-1\n");
+	}
+	else printf("%d\n", queue[head]);	
+}
+void back(){
+	if (head == tail){
+		printf("-1\n");
+	}
+	else printf("%d\n", queue[tail-1]);
+}
+int main(){
+	int n; scanf("%d", &n);
+	char order[10];
+	for (int i = 0 ; i < n ; ++i){
+		scanf("%s", order); 
+		if (!strcmp(order, "push")){
+			int num;
+			scanf("%d", &num);
+			push(num);
+		}
+		else if (!strcmp(order, "pop")){
+			pop();
+		}
+		else if (!strcmp(order, "empty")){
+			empty();
+		}
+		else if (!strcmp(order, "front")){
+			front();
+		}
+		else if (!strcmp(order, "size")){
+			size();
+		}
+		else{
+			back();
+		}
+	}
+	return 0;
+}
