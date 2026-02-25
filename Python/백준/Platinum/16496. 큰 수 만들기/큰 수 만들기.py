9227b77582e69f004f08265ee3adf7ab8141ba7e
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-02-25 20:28:23 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 376 ms, Memory: 30860 KB -BaekjoonHub

---
+++ Python/백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+def sort_(a):
+    for i in range(len(a)-1):
+        idx=i
+        for j in range(i+1, len(a)):
+            if int(a[j]+a[idx])>int(a[idx]+a[j]):
+                idx=j
+        a[idx], a[i]=a[i], a[idx]
+
+n=int(input())
+a=list(input().rstrip().split())
+sort_(a); 
+ans=''.join(a)
+if a.count('0')==len(a): ans='0'
+print(ans)
