439f45e38c9324800d46ea7790734fd8d90d80a8
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-02-25 20:12:34 +0900

    [Silver IV] Title: 로프, Time: 24 ms, Memory: 1900 KB -BaekjoonHub

---
+++ C99/백준/Silver/2217. 로프/로프.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int comp(const void *a, const void * b){
+        return *(int *)b - *(int *)a;
+}
+int maxf(int a, int b){
+        return a > b ? a : b;
+}
+int main() {
+        int N; int i;
+        scanf("%d", &N);
+        int *rope = (int *)malloc(N * sizeof(int));
+        for (i = 0; i < N; i++) {
+                scanf("%d", &rope[i]);
+        }
+        qsort(rope, N, sizeof(int), comp);
+        int max = 0;
+        for (i = 0; i < N; i++) 
+                max = maxf(max, rope[i] * (i+1));
+        printf("%d", max);
+        return 0;
+}
