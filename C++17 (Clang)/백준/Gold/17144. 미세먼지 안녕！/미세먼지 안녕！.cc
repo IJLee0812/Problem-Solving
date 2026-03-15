54d5f5ecbd4b2b2ca712d3f50df0443966a32b39
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-03-15 10:24:12 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 32 ms, Memory: 2220 KB -BaekjoonHub

--- C++17 (Clang)/백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
+++ C++17 (Clang)/백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <cstring>
 #include <cmath>
 #define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0); 
@@ -7,7 +8,7 @@
 using namespace std;
 
 int R, C, T, curr_time = 1;
-short A[MAX][MAX], tempA[MAX][MAX];
+short A[MAX][MAX];
 int D[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
 struct Point{
@@ -18,24 +19,14 @@ struct Purifier{
     Point up, down;
 };
 
+struct Dust{
+    int r, c, val; // val: 미세먼지 수치
+};
+
 Purifier purifier;
 
-void debug_A(){
-    cout << "\n\n";
-
-    cout << "\ncurr_time: " << curr_time << "\n";
-
-    for (int r = 1 ; r <= R ; ++r){
-        for (int c = 1 ; c <= C ; ++c)
-            cout << A[r][c] << ' ';
-        
-        cout << '\n';
-    }
-    cout << "\n\n";
-}
-
 void input(){
-    cin >> R >> C >> T; // R: row, C: col, T: time
+    cin >> R >> C >> T;
 
     bool isUp = false;
     for (int r = 1 ; r <= R ; ++r){
@@ -53,64 +44,53 @@ void input(){
             }
         }
     }
-
-    // cout << "purifier.up.r: " << purifier.up.r << ", purifier.up.c: " << purifier.up.c << '\n';
-    // cout << "purifier.down.r: " << purifier.down.r << ", purifier.down.c: " << purifier.down.c << '\n';
-    // debug_A();
-}
-
-void _diffuse(int r, int c){
-    int cnt = 0;
-
-    int origin = A[r][c], div = origin / 5;
-
-    for (int i = 0 ; i < 4 ; ++i){
-        int nr = r + D[i][0], nc = c + D[i][1];
-        // 공기청정기 칸이 아니면서, 내부 칸
-        if (A[nr][nc] != -1 && nr >= 1 && nr <= R && nc >= 1 && nc <= C){
-            cnt++;
-            tempA[nr][nc] += div;
-        }
-    }
-
-    tempA[r][c] += origin - (div * cnt);
 }
 
 void diffuse(){
-    memset(tempA, 0, sizeof(tempA));
+    vector<Dust> dusts;
 
+    // 1. 현재 맵에 존재하는 모든 미세먼지를 찾아 벡터에 저장
     for (int r = 1 ; r <= R ; ++r)
         for (int c = 1 ; c <= C ; ++c)
-            if (A[r][c] > 0) _diffuse(r, c); 
-        
-    for (int r = 1 ; r <= R ; ++r)
-        for (int c = 1 ; c <= C ; ++c)
-            if (A[r][c] != -1) A[r][c] = tempA[r][c];
+            if(A[r][c] > 0) dusts.push_back({r, c, A[r][c]});
+    
+    // 2. 벡터에 들어있는 먼지들만 꺼내서, 확산 진행
+    for (auto d : dusts){
+        int r = d.r, c = d.c;
+        int div = d.val / 5;
+        int cnt = 0;
+
+        // 원본 값이 4 이하 == 나눌 양 0 == 확산 불필요(skip)
+        if (div == 0) continue;
+
+        for (int i = 0 ; i < 4 ; ++i){
+            int nr = r + D[i][0], nc = c + D[i][1];
+
+            if (A[nr][nc] != -1 && nr >= 1 && nr <= R && nc >= 1 && nc <= C){
+                A[nr][nc] += div;
+                cnt++;
+            }
+        }
+
+        // (중요) 원래 위치에서는, 빠져나간 먼지만큼 빼준다. 이러면 tempA 만들 필요 없이 원본 A를 업데이트하면 됨
+        A[r][c] -= (div * cnt);
+    }
 }
+
 
 void purify(){
     int up_r = purifier.up.r, down_r = purifier.down.r;
 
-    // up = 반시계
-    // 1. 왼쪽 세로줄 (아래로 당기기)
     for (int r = up_r - 1 ; r > 1 ; --r) A[r][1] = A[r - 1][1];
-    // 2. 위쪽 가로줄 (왼쪽으로 당기기)
     for (int c = 1 ; c < C ; ++c) A[1][c] = A[1][c + 1];
-    // 3. 오른쪽 세로줄 (위로 당기기)
     for (int r = 1 ; r < up_r ; ++r) A[r][C] = A[r + 1][C];
-    // 4. 아래쪽 가로줄 (오른쪽으로 당기기)
     for (int c = C ; c > 2 ; --c) A[up_r][c] = A[up_r][c - 1];
 
     A[up_r][2] = 0; // 맑은 공기 처리
 
-    // down = 시계
-    // 1. 왼쪽 세로줄 (위로 당기기)
     for (int r = down_r + 1 ; r < R ; ++r) A[r][1] = A[r + 1][1];
-    // 2. 아래쪽 가로줄 (왼쪽으로 당기기)
     for (int c = 1 ; c < C ; ++c) A[R][c] = A[R][c + 1];
-    // 3. 오른쪽 세로줄 (아래로 당기기)
     for (int r = R ; r > down_r ; --r) A[r][C] = A[r - 1][C];
-    // 4. 위쪽 가로줄 (오른쪽으로 당기기)
     for (int c = C ; c > 2 ; --c) A[down_r][c] = A[down_r][c - 1];
 
     A[down_r][2] = 0; // 맑은 공기 처리
@@ -119,9 +99,7 @@ void purify(){
 void simulation(){
     while (curr_time <= T){
         diffuse();
-        // debug_A();
         purify();
-        // debug_A();
         curr_time++;
     }
 }
