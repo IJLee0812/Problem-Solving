68774d778285ceec603117c8917397d188ad7d49
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-03-21 14:50:50 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2216 KB -BaekjoonHub

---
+++ C++17 (Clang)/백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MAX 1002
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+using namespace std;
+
+int N, dp[MAX], ans;
+vector<int> A;
+
+int main(){
+    fastio
+    cin >> N;
+
+    int tmp;
+    for (int i = 0 ; i < N ; ++i){
+        cin >> tmp;
+        A.push_back(tmp);
+    }
+
+    reverse(A.begin(), A.end());
+
+    for (int i = 0 ; i < N ; ++i){
+        int tmp_max = 0;
+
+        for (int j = 0 ; j < i ; ++j) if (A[i] > A[j]) tmp_max = max(tmp_max, dp[j]);
+
+        dp[i] = tmp_max + 1;
+        ans = max(ans, dp[i]);
+    }
+
+    cout << ans;
+
+    return 0;
+}
