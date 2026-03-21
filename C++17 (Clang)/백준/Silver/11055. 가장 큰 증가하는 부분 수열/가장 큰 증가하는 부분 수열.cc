b0034247fb5cd7c9d90279345de156c1b671b13f
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-03-21 15:39:13 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2220 KB -BaekjoonHub

---
+++ C++17 (Clang)/백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define MAX 1002
+
+using namespace std;
+
+int A[MAX], N, dp[MAX], ans;
+
+int main(){
+    fastio
+    cin >> N;
+
+    for (int i = 1 ; i <= N ; ++i) cin >> A[i];
+
+    for (int i = 1 ; i <= N ; ++i){
+        dp[i] = A[i];
+
+        for (int j = 1 ; j < i ; ++j)
+            if (A[i] > A[j]) dp[i] = max(dp[i], dp[j] + A[i]);
+
+        ans = max(ans, dp[i]);
+    }
+
+    cout << ans;
+    return 0;
+}
