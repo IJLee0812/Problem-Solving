f7d99cf8704ed1adc1523e855f38b57cad465b4e
이익준 <76618299+IJLee0812@users.noreply.github.com>
2026-04-02 20:38:38 +0900

    upload BOJ 2573, 2636 solutions

---
+++ C++/BOJ/BOJ_2573.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <stdlib.h>
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define MAX 302
+
+using namespace std;
+
+int N, M, Graph[MAX][MAX], D[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}, year = 0;
+
+bool visited_check[MAX][MAX];
+
+struct Point{
+    int x, y;
+};
+
+void debug_graph(){
+    cout << "\n\ncurr_year: " << year << "\n\n";
+
+    for (int i = 0 ; i < N ; ++i){
+        for (int j = 0 ; j < M ; ++j){
+            cout << Graph[i][j] << ' ';
+        }
+        cout << "\n\n";
+    }
+}
+
+void input(){
+    cin >> N >> M;
+
+    for (int i = 0 ; i < N ; ++i)
+        for (int j = 0 ; j < M ; ++j)
+            cin >> Graph[i][j];
+}
+
+void checkBFS(int i, int j){
+    queue<Point> myqueue;
+    myqueue.push({i, j});
+    visited_check[i][j] = true;
+
+    while (!myqueue.empty()){
+        Point curr = myqueue.front();
+
+        myqueue.pop();
+
+        for (int k = 0 ; k < 4 ; ++k){
+            int nx = curr.x + D[k][0], ny = curr.y + D[k][1];
+
+            if (nx >= 0 && nx < N && ny >= 0 && ny < M && Graph[nx][ny] > 0 && !visited_check[nx][ny]){
+                myqueue.push({nx, ny});
+                visited_check[nx][ny] = true;
+            }
+        }
+    }
+}
+
+int melt(int x, int y){
+    int melt_amount = 0;
+    for (int i = 0 ; i < 4 ; ++i){
+        int nx = x + D[i][0], ny = y + D[i][1];
+
+        if (nx >= 0 && nx < N && ny >= 0 && ny < M && (Graph[nx][ny] == 0)) melt_amount++;        
+    }
+
+    return melt_amount;
+}
+
+void simulation(){
+    // 동시성 처리 필요(1이 녹아서 0이 되도, 같은 년도에는 옆 빙산조각한테 반영되면 안 됨)
+    queue<pair<Point, int>> myqueue;
+
+    for (int i = 0 ; i < N ; ++i){
+        for (int j = 0 ; j < M ; ++j){
+            if (Graph[i][j] > 0){
+                int melt_amount = melt(i, j);
+                myqueue.push({{i, j}, melt_amount});
+            }
+        }
+    }
+
+    while(!myqueue.empty()){
+        Point curr = myqueue.front().first;
+        int melt_amount = myqueue.front().second;
+        
+        myqueue.pop();
+
+        // 깎인 빙산 높이가 음수가 되면 안 됨.
+        Graph[curr.x][curr.y] = max(0, Graph[curr.x][curr.y] - melt_amount);
+    }
+}
+
+int main(){
+    fastio
+    input();
+
+    while (true){
+        memset(visited_check, false, sizeof(visited_check));
+
+        int component_cnt = 0; // 덩어리 개수
+
+        for (int i = 0 ; i < N ; ++i){
+            for (int j = 0 ; j < M ; ++j){
+                if (Graph[i][j] > 0 && !visited_check[i][j]){
+                    checkBFS(i, j);
+                    component_cnt++;
+                }
+            }
+        }
+
+        if (component_cnt >= 2){
+            cout << year;
+            return 0;
+        }
+
+        if (component_cnt == 0){
+            cout << '0';
+            return 0;
+        }
+
+        // 체크 완료되면, 녹이기 시뮬레이션
+        simulation();
+        year++;
+        // debug_graph();
+    }
+
+    return 0;
+}
---
+++ C++/BOJ/BOJ_2636.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cstring>
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define MAX 102
+
+using namespace std;
+
+struct Point{
+    int x, y;
+};
+
+int Graph[MAX][MAX], D[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}, N, M, hour = 0, cheese_count = 0;
+
+bool visited[MAX][MAX], isAllMelted = false;
+
+vector<Point> melting_cheese_pieces;
+
+void debug_graph(){
+    cout << "\n\n" << "curr hour: " << hour << '\n';
+    for (int i = 0 ; i < N ; ++i){
+        for (int j = 0 ; j < M ; ++j){
+            cout << Graph[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    cout << "\n\n";
+}
+
+void input(){
+    cin >> N >> M; // N: 세로, M: 가로
+
+    for (int i = 0 ; i < N ; ++i)
+        for (int j = 0 ; j < M ; ++j)
+            cin >> Graph[i][j];
+}
+
+bool check(){
+    queue<Point> myqueue;
+    memset(visited, false, sizeof(visited));
+    melting_cheese_pieces.clear();
+
+    myqueue.push({0, 0});
+    visited[0][0] = true;
+    bool isExistCheese = false;
+
+    while (!myqueue.empty()){
+        Point curr = myqueue.front();
+        myqueue.pop();
+
+        for (int i = 0 ; i < 4 ; ++i){
+            int nx = curr.x + D[i][0], ny = curr.y + D[i][1];
+
+            if (nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny]){
+                visited[nx][ny] = true;
+                
+                if (!Graph[nx][ny]) myqueue.push({nx, ny});
+                else{
+                    isExistCheese = true;
+                    melting_cheese_pieces.push_back({nx, ny});
+                }
+            }
+        }
+    }
+
+    return isExistCheese;
+}
+
+void melt(){
+    for (const auto& piece : melting_cheese_pieces) Graph[piece.x][piece.y] = 0;
+}
+
+void simulation(){
+    int current_cheese = 0;
+    
+    // 치즈먼저 세기
+    for (int i = 0 ; i < N ; ++i)
+        for (int j = 0 ; j < M ; ++j)
+            if (Graph[i][j]) current_cheese++;
+
+    if (!current_cheese){
+        cout << hour << '\n' << cheese_count; // cheese_count: 1시간 전 치즈개수
+        isAllMelted = true;
+        return;
+    }
+
+    cheese_count = current_cheese; // 전역변수 갱신
+
+    check();
+    melt();
+    hour++;
+    // debug_graph();
+}
+
+int main(){
+    fastio
+    input();
+
+    while (!isAllMelted) simulation();
+
+    return 0;
+}
