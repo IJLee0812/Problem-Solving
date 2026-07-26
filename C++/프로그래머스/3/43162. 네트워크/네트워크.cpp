c13042441121aa118cbdc203f9ca94c3c6c843a7
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-07-26 11:12:08 +0900

    [level 3] Title: 네트워크, Time: 0.04 ms, Memory: 4.95 MB -BaekjoonHub

--- C++/프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ C++/프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,33 +1,32 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <queue>
 
 using namespace std;
 
-void BFS(int start_node, int n, vector<bool>& visited, const vector<vector<int>>& computers){
+void BFS(int target, int n, vector<bool> &visited, const vector<vector<int>> &computers){
     queue<int> myqueue;
     
-    myqueue.push(start_node);
-    visited[start_node] = true;
+    visited[target] = true;
+    myqueue.push(target);
     
     while (!myqueue.empty()){
         int curr = myqueue.front();
         myqueue.pop();
         
         for (int i = 0 ; i < n ; ++i){
-            if (curr != i && computers[curr][i] && !visited[i]){
+            if (!visited[i] && i != target && computers[curr][i]){
                 visited[i] = true;
                 myqueue.push(i);
             }
         }
-    } 
+    }
 }
 
 int solution(int n, vector<vector<int>> computers) {
-    vector<bool> visited(n, false); // 선언 + false 초기화
-
     int answer = 0;
+    
+    vector<bool> visited(n, false);
     
     for (int i = 0 ; i < n ; ++i){
         if (!visited[i]){
