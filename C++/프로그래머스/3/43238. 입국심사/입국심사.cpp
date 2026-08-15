34b759fba7a33ea4ae254311da9cc3962f54fef6
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-08-15 23:18:24 +0900

    [level 3] Title: 입국심사, Time: 20.73 ms, Memory: 6.96 MB -BaekjoonHub

--- C++/프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ C++/프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -4,28 +4,22 @@
 
 using namespace std;
 
-long long answer = 0;
-
-void binarySearch(int n, const vector<int>& times, long long left, long long right){
-    // 재귀 종료조건
-    if (left > right) return;
-    
-    long long mid = (left + right) / 2, possible_n = 0;
-    for (int i = 0 ; i < times.size() ; ++i) possible_n += mid / times[i];
-    
-    if (possible_n >= n){
-        answer = mid;
-        binarySearch(n, times, left, mid - 1);
-    }
-    else binarySearch(n, times, mid + 1, right);
-}
-
 long long solution(int n, vector<int> times) {
-    long long left = 1, right;
     sort(times.begin(), times.end());
-    right = (long long) times[times.size() - 1] * n;
     
-    binarySearch(n, times, left, right);
+    long long answer = 0, left = (long long) times[0], right = (long long) times[times.size() - 1] * n;
+    
+    while (left <= right){
+        long long mid = (left + right) / 2, possible_n = 0;
+        
+        for (int i = 0 ; i < times.size() ; ++i) possible_n += mid / times[i];
+        
+        if (possible_n >= n){
+            answer = mid;
+            right = mid - 1;
+        }
+        else left = mid + 1;
+    }
     
     return answer;
 }
