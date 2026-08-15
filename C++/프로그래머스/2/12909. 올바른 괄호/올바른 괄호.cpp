a367ac2e11fcec543958fe169cc7cf47a066c8a6
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-08-15 17:50:49 +0900

    [level 2] Title: 올바른 괄호, Time: 0.36 ms, Memory: 4.76 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    stack<char> mystack;
+    
+    for (char ch : s){
+        if (ch == '(') mystack.push(ch);
+        else if (ch == ')'){
+            if (mystack.empty()) return false;
+            mystack.pop();
+        }
+    }
+    
+    return mystack.empty();
+}
