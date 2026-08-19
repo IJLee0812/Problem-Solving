0cf3c30471824b910b86a3a8d829d8e8ae098eea
Ikjun Lee <ikjun.lee@partridgesystems.com>
2026-08-19 23:15:24 +0900

    [level 4] Title: 징검다리, Time: 3.93 ms, Memory: 5.47 MB -BaekjoonHub

--- C++/프로그래머스/4/43236. 징검다리/징검다리.cpp
+++ C++/프로그래머스/4/43236. 징검다리/징검다리.cpp
@@ -1,39 +1,35 @@
-#include <string>
-#include <vector>
 #include <algorithm>
-#include <cmath>
+#include <vector>
 
 using namespace std;
 
-int answer = 0;
-
-void binarySearch(const vector<int>& rocks, int n, int left, int right){
-    int _distance = right;
+int binarySearch(int distance, const vector<int>& rocks, int n){
+    int left = 1, right = distance, res = 0;
+    
     while (left <= right){
         int mid = (left + right) / 2;
         int destroy_count = 0, prev_pos = 0;
         
-        for (int i = 0 ; i < rocks.size() ; ++i){
-            int dist = rocks[i] - prev_pos;
-            
-            if (dist < mid) destroy_count++;
-            else prev_pos = rocks[i];
+        for (int rock : rocks){
+            if (rock - prev_pos < mid) destroy_count++;
+            else prev_pos = rock;
         }
         
-        // 마지막 위치(prev_pos)에서, 도착 지점(distance) 까지의 거리도 체크해야 됨.
-        if (_distance - prev_pos < mid) destroy_count++;
-        
+        // 마지막 위치(prev_pos)에서, 도착 지점(distance) 까지의 거리
+        if (distance - prev_pos < mid) destroy_count++;
         
         if (destroy_count > n) right = mid - 1;
         else{
-            answer = max(answer, mid);
+            res = mid; // 정답 갱신해버리기
             left = mid + 1;
         }
     }
+    
+    return res;
 }
 
 int solution(int distance, vector<int> rocks, int n) {
     sort(rocks.begin(), rocks.end());
-    binarySearch(rocks, n, 0, distance); // rocks, n, init {left, right}
-    return answer;
+    
+    return binarySearch(distance, rocks, n);
 }
